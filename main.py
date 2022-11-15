71d2724e41a27f177826c301d1e0eb4c9dfd8537
jp3pe <rlaworhkd430430@gmail.com>
2022-11-15 20:11:55 +0900

    add: 생성한 UI 파일 출력하는 기능 추가
    closes #3

--- main.py
+++ main.py
@@ -1,34 +1,25 @@
 import sys
-import random
-from PySide6 import QtCore, QtWidgets, QtGui
 
-
-class MyWidget(QtWidgets.QWidget):
-    def __init__(self):
-        super().__init__()
-
-        self.hello = ["Hallo Welt", "Hei maailma", "Hola Mundo", "Привет мир"]
-
-        self.button = QtWidgets.QPushButton("Click me!")
-        self.text = QtWidgets.QLabel("Hello World",
-                                     alignment=QtCore.Qt.AlignCenter)
-
-        self.layout = QtWidgets.QVBoxLayout(self)
-        self.layout.addWidget(self.text)
-        self.layout.addWidget(self.button)
-
-        self.button.clicked.connect(self.magic)
-
-    @QtCore.Slot()
-    def magic(self):
-        self.text.setText(random.choice(self.hello))
-
+from PySide6.QtUiTools import QUiLoader
+from PySide6.QtWidgets import QApplication
+from PySide6.QtCore import QFile, QIODevice
 
 if __name__ == "__main__":
-    app = QtWidgets.QApplication([])
+    app = QApplication(sys.argv)
+    ui_file_name = "calculator-window.ui"
+    ui_file = QFile(ui_file_name)
 
-    widget = MyWidget()
-    widget.resize(800, 600)
-    widget.show()
+    if not ui_file.open(QIODevice.ReadOnly):
+        print(f"Cannot open {ui_file_name}: {ui_file.errorString()}")
+        sys.exit(-1)
+
+    loader = QUiLoader()
+    window = loader.load(ui_file)
+    ui_file.close()
+
+    if not window:
+        print(loader.errorString())
+        sys.exit(-1)
+    window.show()
 
     sys.exit(app.exec())
